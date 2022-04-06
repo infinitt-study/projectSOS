79a0b6a565b9dee1d1cde18064ffddc335e9d7f9
leesangh <leesangh92@gmail.com>
2022-04-06 15:24:55 +0900

    admin

--- projectSOS_Method.cpp
+++ projectSOS_Method.cpp
@@ -71,30 +71,48 @@ public:
 
 		cout << "ID 입력 : ";
 		cin >> _ID;
-
 		cout << "PW 입력 : ";
 		cin >> _PW;
 
-		if (_ID != "admin") {
-			cout << "잘못된 ID 입력" << endl;
+		if (strcmp(_ID, "admin") != 0) {
+			cout << "잘못된 admin ID 입력" << endl;
 			return false;
 		}
-		
-		if (_PW != "1234") {
-			cout << "잘못된 PW 입력" << endl;
+		if (strcmp(_PW, "1234") != 0) {
+			cout << "잘못된 admin PW 입력" << endl;
 			return false;
 		}
+		cout << "'관리자' 권한으로 로그인 되었습니다." << endl;
 		return true;
 	}
-
-	admin_Input(const int _PK, const char *_name, const char *_ID, const int _birth)
+	/*void admin_Input(const int _PK, const char* _name, const char* _ID, const int _birth)
 	{
+		int _PK = 0;
+		char name[20], ID[20], birth[20];
 
-	}
+		for (; _PK < ; _PK++)
+		{
+			cout << "환자 이름(성명)을 입력해주세요. : ";
+			cin >> name;
 
-	admin_Output(PK, name, ID, birth)
+			if (strcmp(name, "end") == 0) {
+				break;
+			
+
+			cout << "ID를 입력해주세요. : ";
+			cin >> ID;
+			cout << "생년월일을 입력해주세요.(ex.890101) : ";
+			cin >> birth;
+		}
+
+	}*/
+	void admin_Output(const int _PK, const char *_name, const char *_ID, const int birth)
 	{
-
+		for (; _PK <  ; _PK++) {
+			cout << "환자의 이름(성명) : " << _name << "\n" 
+				<< _name << " 환자의 ID : " << _ID << "\n"
+				<< "환자의 생년월일 : " << birth;
+		}
 	}
 };
 
@@ -105,7 +123,7 @@ int main() {
 
 	//로그인 확인
 	while (!login) {
-		cout << "접근 권한 선택 : ";
+		cout << "접근 권한 선택 [ 1 : 관리자(admin), 2 : 환자(patient), 3 : 의사(doctor) ] : ";
 		cin >> role;
 		
 		switch (role) {
@@ -116,7 +134,12 @@ int main() {
 			break;
 		case 3: //doc
 			break;
+		default:
+			cout << "잘못된 입력입니다." << endl;
+			break;
 		}
+		cin.clear(); // 문자열 비우기
+		cin.ignore(); // 버퍼 제거
 	}
 
 	// 로그인 되었을 경우 작성
